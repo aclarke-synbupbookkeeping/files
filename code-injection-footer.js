f0510458f6654bc5c0dc06f7b8e4c112537ffb61
Andrew Clarke <andrew.clarke@syncupbookkeeping.com>
2025-02-13 15:58:19 -0800

    Initial upload of these files - 13-FEB-2025 15:55

---
+++ code-injection-footer.js
@@ -0,0 +1,38 @@
+<!-- BACK TO TOP BUTTON
+<a id="top-button" style="text-align:center;" href="#page">TOP</a>
+<style>
+  html { scroll-behavior: smooth; }
+  #top-button {
+    position: fixed;
+    z-index: 100;
+    bottom: 80px;
+    right: 20px;
+    width: 40px;
+    height: 40px;
+    background: #fff;
+    border: 1px solid #000;
+  }
+
+  #top-button:before {
+    font-family: 'squarespace-ui-font';
+    content: "\e006";
+    color: #000;
+    font-size: 30px;
+    width: 100%;
+    height: 100%;
+    position: absolute;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+  }
+</style> -->
+
+<div>
+	<table class="table-footer">
+		<tr>
+			<td><a href="https://www.syncupbookkeeping.com/policies">Our Policies</a></td>
+			<td><a href="#" class="termly-display-preferences">Consent Preferences</a></td>
+			<td><a href="https://app.termly.io/notify/8a76a0da-013f-4d03-8839-16687ea142b1" target="_blank">Limit the Use Of My Sensitive Personal Information</a></td>
+			<td><a href="https://app.termly.io/notify/8a76a0da-013f-4d03-8839-16687ea142b1" target="_blank">Do Not Sell or Share My Personal information</a></td>
+		</tr>
+	</table>

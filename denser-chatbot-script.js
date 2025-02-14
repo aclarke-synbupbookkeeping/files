fa333cce261ffc32ce7755b474b1c0a11e2a0d53
Andrew Clarke <andrew.clarke@syncupbookkeeping.com>
2025-02-13 16:03:08 -0800

    Add files via upload
    Initial upload of these files - 13-FEB-2025 15:55

---
+++ denser-chatbot-script.js
@@ -0,0 +1,13 @@
+<script type="module">
+  import Chatbot from "https://cdn.jsdelivr.net/npm/@denserai/embed-chat@1/dist/web.min.js";
+ 
+  Chatbot.init({
+    chatbotId: "5a9e92f9-76ae-4102-b09a-6dbbab5b4d23",
+    theme: {
+      button: {
+        iconCover: true,
+        size: "large"
+      },
+    },
+  });
+</script>

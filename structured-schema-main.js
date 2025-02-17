d4a3a2fe46b78e1f4f21ab3652542d07938e295a
Andrew Clarke <andrew.clarke@syncupbookkeeping.com>
2025-02-16 19:19:54 -0800

    Initial Upload 16-FEB-2025 19:20

---
+++ structured-schema-main.js
@@ -0,0 +1,59 @@
+<script type="application/ld+json">
+{
+  "@context": "https://schema.org",
+  "@type": "AccountingService",
+  "@id": "BookkeeperFirm",
+  "additionalType": ["BookkeepingService","Bookkeeping Service","Bookkeeping","Bookkeeper","Bookkeeping Firm","AccountingServices","Accounting Services"],
+  "keywords": ["Accounting","Accounting Services","Financial Reporting", "Financial Reports","Analysis","Business Analysis","Business Consulting","Bookkeeping","San Diego Bookkeeper","San Diego Bookkeeping","Best Bookkeeper in San Diego","San Diego Accounting","San Diego Accounting Services"],
+  "areaServed": ["San Diego","San Diego, CA","San Diego County","San Diego Metro","Metro San Diego","San Diego Metropolitan","Greater San Diego","Southern California","North County","Gas Lamp","North Park","South Park","East Village","Mission Valley","Mission Bay","Mission Beach","Point Loma","Kearny Mesa","Chula Vista","East Lake","La Mesa","El Cajon","Bonita","Coronado","La Jolla","Clairemont","Carlsbad","Carmel Valley","Torrey Pines","Pacifc Beach","Ocean Beach","Del Mar","Ocean Beach","PB","OB"],
+  "name": "Sync-Up Bookkeeping",
+  "alternateName": ["Sync-Up Bookkeeping","Sync-Up","Sync Up Bookkeeping","SUP Bookkeeping", "BEANS AND BYTES TECHNICAL SERVICES LLC", "BEANS AND BYTES TECHNICAL SERVICES, LLC", "BEANS AND BYTES", "Sync-Up"],
+  "image": [
+        "https://images.squarespace-cdn.com/content/v1/66638aa0dcac9f7f9728dd43/cc9e40b8-5430-488d-9ad6-d69ca5a44dec/Logo+Rounded+Edges+622x754.png?format=1500w","https://images.squarespace-cdn.com/content/v1/66638aa0dcac9f7f9728dd43/c068934a-ba7c-4e38-894f-095733ed6c3f/Sheri_Headshot_614x514.PNG?format=2500w","https://images.squarespace-cdn.com/content/v1/66638aa0dcac9f7f9728dd43/11b31868-1e9e-4d9a-b86c-c2aae2ebf00d/san-diego-gas-lamp-sign.png?format=2500w","https://images.squarespace-cdn.com/content/v1/66638aa0dcac9f7f9728dd43/d78f1b24-00be-4f0b-bde3-08fe74aa1e2d/african-american-woman-holding-syncup-reports.PNG?format=2500w"
+       ],
+  "logo": "https://images.squarespace-cdn.com/content/v1/66638aa0dcac9f7f9728dd43/cc9e40b8-5430-488d-9ad6-d69ca5a44dec/Logo+Rounded+Edges+622x754.png?format=1500w",
+  "url": "https://www.syncupbookkeeping.com",
+  "telephone": "8887884512",
+  "email": "info@syncupbookkeeping.com",
+  "naics": "541219",
+  "priceRange": "$$$",
+  "legalName": "BEANS AND BYTES TECHNICAL SERVICES LLC DBA SYNC-UP BOOKKEEPING",
+  "foundingDate": "2024-06-04",
+  "duns": "13-4878634",
+  "taxID": "33-2294661",
+  "knowsLanguage": "US-EN",
+  "paymentAccepted": ["Credit Card","Debit Card","Mastercard","Visa","American Express","Discover"],
+  "address": {
+    "@type": "PostalAddress",
+    "streetAddress": "3916 North Potsdam Avenue PMB 5079",
+    "addressLocality": "Sioux Falls",
+    "addressRegion": "SD",
+    "postalCode": "57104",
+    "addressCountry": "US"
+  },
+  "geo": {
+    "@type": "GeoCoordinates",
+    "latitude": 43.5905969,
+    "longitude": -96.694682
+  },
+  "openingHoursSpecification": {
+    "@type": "OpeningHoursSpecification",
+    "dayOfWeek": [
+      "Monday",
+      "Tuesday",
+      "Wednesday",
+      "Thursday",
+      "Friday"
+    ],
+    "opens": "08:00",
+    "closes": "16:00"
+  },
+  "sameAs": [
+    "https://www.facebook.com/61565534363316",
+    "https://www.instagram.com/syncupbookkeeping",
+    "https://www.youtube.com/@SyncUpBookkeeping",
+    "https://www.linkedin.com/company/syncupbookkeeping",
+    "https://www.syncupbookkeeping.com/local/san-diego-bookkeeping"
+  ]
+}
+</script>
